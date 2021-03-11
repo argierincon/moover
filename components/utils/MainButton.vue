751a178c814f549af06274b15fd7d99a545fb982
argierincon <argierincon@gmail.com>
2021-03-11 09:08:12 -0500

    Anadiendo componente main button

---
+++ components/utils/MainButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="main-button">
+    {{ textBtn }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    textBtn: {
+      type: String,
+      default: "Click",
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.main-button
+  width: 170px
+  height: 40px
+  margin: 30px 0
+  background-color: $color-primary
+  border: none
+  border-radius: 5px
+  color: $color-light
+  font-weight: 500
+  font-family: poppins
+  text-transform: uppercase
+  cursor: pointer
+
+</style>
