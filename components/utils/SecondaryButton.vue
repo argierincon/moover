001350913fbdc3e455ee0ee8df6dd395d37d7520
argierincon <argierincon@gmail.com>
2021-03-11 09:08:21 -0500

    Anadiendo componente Secondary button

---
+++ components/utils/SecondaryButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <Button :class="`secondary-button ${type}`">
+    {{ textBtn }}
+  </Button>
+</template>
+
+<script>
+export default {
+  props: {
+    textBtn: {
+      type: String,
+      default: "Click",
+    },
+    type: {
+      type: String,
+      default: "green",
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.secondary-button
+  width: 170px
+  height: 40px
+  background: none
+  border-radius: 5px
+  font-weight: 500
+  font-family: poppins
+  text-transform: uppercase
+  cursor: pointer
+
+.green
+  border: 2px solid #32D6B6
+  color: #32D6B6
+
+.purple
+  border: 2px solid #505AF6
+  color: #505AF6
+
+@media (min-width: 768px)
+  .secondary-button
+    margin: 10px 0
+</style>
