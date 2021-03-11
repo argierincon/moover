1f8ca1dbae35594503431920e1ead81fc0254799
argierincon <argierincon@gmail.com>
2021-03-11 09:06:23 -0500

    Anadiendo Index

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Banner />
+    <AboutUs />
+    <HowItWorks />
+  </div>
+</template>
+
+<script>
+import ContactUs from "../components/home/ContactUs";
+import Banner from "../components/home/Banner";
+import AboutUs from "../components/home/AboutUs";
+import HowItWorks from "../components/home/HowItWorks";
+
+export default {
+  components: {
+    Banner,
+    AboutUs,
+    HowItWorks,
+  },
+};
+</script>
